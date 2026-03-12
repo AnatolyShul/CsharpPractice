e32f6c2d0a12b3aa58c98994e76dec046e8273f8
AnatolyShul <tolik10122007@mail.ru>
2026-03-12 17:26:20 +0300

    Update and rename temp.txt to Animal.cs

---
+++ Практика 12.03.26/Animal.cs
@@ -0,0 +1,41 @@
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace Class
+{
+    public abstract class Animal
+    {
+        private int _id;
+
+        public int Id => _id;
+        public abstract string Type {get;}
+        public Animal(int id)
+        {
+            _id = id;
+        }
+
+        public abstract void Voice();
+
+        public virtual void Print()
+        {
+            System.Console.Write("Animal ID " + _id);
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override string Type => "Кошка";
+        public Cat(int id) : base(id) { }
+        public override void Voice() => System.Console.WriteLine("meow");
+        public void Idol()
+        {
+            System.Console.WriteLine(Id);
+        }
+        public override void Print()
+        {
+            Console.WriteLine("кошка");
+        }
+    }
+}
+
