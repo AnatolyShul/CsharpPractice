8114cdc624c9c9f43caf232260a8ea3cd645e39e
AnatolyShul <tolik10122007@mail.ru>
2026-03-12 17:41:03 +0300

    Update Program.cs

--- 26.02.26/Program.cs
+++ 26.02.26/Program.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Runtime.Intrinsics.Arm;
 
+namespace ClassWork
+{
+    public class Program
+    {
+        static void Main()
+        {
+            A a = new A("Роберт", "Пренко", 18, 150);
+
+            B b = new ("Тимоха", "Сагидуллин", 18, -20, 2, false);
+            b.Print();
+        }
+    }
+}
