1035695a38723be39a57ea970b9be3f7b118a088
Анатолий <tolik10122007@gmail.com>
2026-05-07 16:59:33 +0300

    07.05

---
+++ 07.05.2026/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Serialization;
+
+namespace XMLinClass
+{
+    public class Movie
+    {
+        private string _name;
+        private int _duration;
+        public string Name => _name;
+        public int Duration => _duration;
+
+        public Movie(string name, int duration)
+        {
+            _name = name;
+            _duration = duration;
+        }
+    }
+
+    public class MovieDTO
+    {
+        //свойства с публичными сеттерами
+        public string Name{ get; set; }
+        public int Duration{ get; set; }
+
+        //конструктор без параметров
+        public MovieDTO() { }
+
+        //Movie -> MovieDTO
+
+        public MovieDTO(string name, int duration)
+        {
+            Name = name;
+            Duration = duration;
+        }
+
+        public MovieDTO(Movie movie)
+        {
+            Name = movie.Name;
+            Duration = movie.Duration;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Movie movie1 = new("Harry Poter", 120);
+
+            MovieDTO movieDTO1 = new(movie1);
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(folderPath, "movie.xml");
+
+            var serializer = new XmlSerializer(typeof(MovieDTO));
+        }
+    }
+}
